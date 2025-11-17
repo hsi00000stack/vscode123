e505578537265d8ed46e66f1e05694d97c050e81
hsi00000stack <hsi03042@gmail.com>
2025-11-17 10:35:44 +0900

    ...

--- day11/ex02/index.js
+++ day11/ex02/index.js
@@ -1,5 +1,24 @@
 export default async function Main() {
-
     console.log('hello js')
     
+        var a = 1
+    console.log(a)
+    a = 7
+    console.log(a)
+
+    let b=1
+    console.log(b)
+    b= 8 + a
+    console.log(b)
+
+    var a = 2;
+    console.log(a)
+    
+    b = 3
+    console.log(b)
+
+    const c =1
+    console.log(c)
+    c=3
+    console.log(c)
 }
