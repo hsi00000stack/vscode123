4eb9d7a06da31af015694904e0607c900c46c9f8
hsi00000stack <hsi03042@gmail.com>
2025-11-17 10:34:33 +0900

    ...

---
+++ day12/ex01/index.js
@@ -0,0 +1,38 @@
+export default async function Main() {
+    console.log('hello js')
+    
+    let currentIndex = 0;
+    const menuItems = document.querySelectorAll(".menu-item");
+
+    console.log(menuItems)
+
+    menuItems[currentIndex].classList.add("select")
+
+    window.addEventListener("keydown", (e) => {
+    
+        console.log(e.key)
+
+        menuItems [currentIndex].classList.remove("select")
+
+        if (e.key == "ArrowUp") {
+            currentIndex--
+            if(currentIndex < 0) {
+                currentIndex = 2
+            }
+        
+        }
+        else if(e.key == "ArrowDown") {
+            currentIndex++
+            currentIndex %=3
+        }
+        console.log(currentIndex)
+            
+
+    })
+    
+
+
+
+
+     
+}
